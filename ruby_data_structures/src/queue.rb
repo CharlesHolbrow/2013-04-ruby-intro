b7efe5aafbf163866573c499d86ac60310f94118
Hack Reactor Students <github@hackreactor.com>
2013-05-24 15:50:03 -0700

    Add ruby queue class

--- ruby_data_structures/src/queue.rb
+++ ruby_data_structures/src/queue.rb
@@ -1,15 +1,48 @@
+class Node
+
+  attr_accessor :next
+  attr_accessor :prev
+  attr_accessor :value
+
+  def initialize value
+    @value = value
+  end
+
+  def to_s
+    @value
+  end
+
+  def inspect
+    'Node: ' << @value
+  end
+
+end
+
+
 class Queue
 
   def initialize
-    @storage = []
+    @head = nil
+    @tail = nil
   end
 
-  def push item
-    @storage.push item
+  def push value
+    new_node = Node.new(value)
+    if @head then
+      @head.next = new_node
+      @head = new_node
+    else
+      @head = new_node
+      @tail = new_node
+    end
+    value
   end
 
   def get
-    @storage.unshift
+    return unless @tail
+    value = @tail.value
+    @tail = @tail.next
+    value
   end
 
 end
